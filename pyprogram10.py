fc1e347a83e299424c1e23b0a25a6c9fbbf3b393
bipin <bipinhyoju@gmail.com>
2025-06-02 19:26:45 +0545

    first commit

---
+++ pyprogram10.py
@@ -0,0 +1,9 @@
+def main():
+    length1 = int(input("What is the length of side 1? "))
+    length2 = int(input("What is the length of side 2? "))
+    length3 = float(input("What is the length of side 3? "))
+
+    print("The perimeter of the triangle is", length1 + length2 + length3)
+
+if __name__ == '__main__':
+    main()
