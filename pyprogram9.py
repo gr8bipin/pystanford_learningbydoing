92cb8fd1a647e3ca393e03a9891bf5e455d9a658
bipin <bipinhyoju@gmail.com>
2025-06-02 19:02:36 +0545

    first commit

---
+++ pyprogram9.py
@@ -0,0 +1,7 @@
+def main():
+    number = input("Type a number to see its square: ")
+    number = float(number)
+    print(number,"squared is", number * number)
+
+if __name__ == '__main__':
+    main()
