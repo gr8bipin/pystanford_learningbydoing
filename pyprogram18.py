547bdd0126227adb684b832ebf58a1cf86e53fa0
bipin <bipinhyoju@gmail.com>
2025-06-04 21:40:06 +0545

    Second Commit

--- pyprogram18.py
+++ pyprogram18.py
@@ -4,19 +4,10 @@ def main():
     """
     Makes Karel place beepers in a square (4 beepers total) and end in the same position Karel starts in.
     """
-
-    put_beeper()
-    move()
-    turn_left()
-    put_beeper()
-    move()
-    turn_left()
-    put_beeper()
-    move()
-    turn_left()
-    put_beeper()
-    move()
-    turn_left()
+    for i in range(4):
+        put_beeper()
+        move()
+        turn_left()
 
 if __name__ == '__main__':
     main()
