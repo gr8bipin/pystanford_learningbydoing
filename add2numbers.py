a4e0cf7eb1a0715fc9407df99eb79cda3d92b694
bipin <bipinhyoju@gmail.com>
2025-06-08 12:21:11 +0545

    First Commit

---
+++ add2numbers.py
@@ -0,0 +1,9 @@
+def main():
+    print("This program adds two numbers.")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    total = num1 + num2
+    print("The sum of two numbers is " + str(total) + ".")
+
+if __name__ == '__main__':
+    main()
