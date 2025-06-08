3098fb896959f1a6e49f2aaa66edd320ef1727af
bipin <bipinhyoju@gmail.com>
2025-06-08 13:28:28 +0545

    First commit

---
+++ constants.py
@@ -0,0 +1,12 @@
+# convert feet into inch
+
+INCHES_IN_FOOT = 12
+
+def main():
+    
+    feet = float(input("Enter number of feet: "))
+    inches = feet * INCHES_IN_FOOT
+    print("That is", inches, "inches!")
+
+if __name__ == '__main__':
+    main()
