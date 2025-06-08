9bc5a6f1569e1632aae067ff1f51ddd045027eba
bipin <bipinhyoju@gmail.com>
2025-06-08 13:54:29 +0545

    First Commit

---
+++ squareroot.py
@@ -0,0 +1,11 @@
+# compute squareroots
+
+import math
+
+def main():
+    number = float(input("Enter a number: "))
+    root = math.sqrt(number)
+    print("The squareroot of number is", root)
+
+if __name__ == '__main__':
+    main()
